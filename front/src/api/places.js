e9fe4960cdd233adc89e5756b8f271d5698bdd6a
gaziza13 <gazeka.04@mail.ru>
2026-02-24 23:53:09 +0500

    deploy for front

--- front/src/api/places.js
+++ front/src/api/places.js
@@ -1,6 +1,6 @@
 import api from "./axios";
 
-const API_BASE = import.meta.env.VITE_API_BASE || "http://127.0.0.1:8000";
+const API_BASE = import.meta.env.VITE_API_BASE;
 
 export const fetchInspirationPlaces = async (
   page = 1,
