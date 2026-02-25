0afe145f07f3e607b8cb35b6a6a8a4f8224fd2f3
gaziza13 <gazeka.04@mail.ru>
2026-02-26 00:00:20 +0500

    fixed cors

--- back/bizbenSayahatta/settings.py
+++ back/bizbenSayahatta/settings.py
@@ -74,7 +74,7 @@ MIDDLEWARE = [
 #CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "https://bizbensayahatta1.vercel.app",
+    "https://bizbensayahatta.vercel.app",
     "https://bizbensayahatta.onrender.com",
 ]
 
